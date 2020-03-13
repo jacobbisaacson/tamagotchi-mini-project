8fe3cecf1ed92bc91bde339aa83d814f68202330
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-13 09:47:34 -0500

    get older

--- js/app.js
+++ js/app.js
@@ -31,14 +31,21 @@ const play = {
   sleep: function() {
     if(this.sleepy === true) {
       this.sleepy = false
+      this.bored = true
+      this.getOlder()
       console.log("im tired! going to sleep");
     } else {
       console.log("just woke up, ready for the day!");
     }
+  },
+
+  getOlder: function() {
+    this.age ++
+    console.log("just got 1 year older");
   }
 
 
-
+// need to set the name -- make a form
 
 
 
