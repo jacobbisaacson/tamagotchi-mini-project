7935298df70f8dfb82bca576f6ed733860f3fea7
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-13 09:36:55 -0500

    class setup and feed function

---
+++ js/app.js
@@ -0,0 +1,42 @@
+console.log("tamagotchi");
+
+class Tamagotchi {
+  constructor() {
+	this.hungry = false
+	
+	this.bored = false
+
+	this.sleepy = false
+
+	this.age = 0
+
+	this.name = name
+    
+  }
+
+}
+
+const play = {
+  
+  feed: function() {
+  	if(this.hungry === true) {
+  		this.hungry = false
+  		this.sleepy = true
+  		console.log("feed me!");
+  	} else {
+  		console.log("just ate");
+  	}
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+ }
